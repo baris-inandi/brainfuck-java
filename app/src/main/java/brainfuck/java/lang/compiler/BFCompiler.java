11a88da4d0c1f069b98d94ade8cd67baa07551b1
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-09-19 14:05:59 +0300

    commit

--- app/src/main/java/brainfuck/java/lang/compiler/BFCompiler.java
+++ app/src/main/java/brainfuck/java/lang/compiler/BFCompiler.java
@@ -1,13 +1,11 @@
 package brainfuck.java.lang.compiler;
 
 import brainfuck.java.lang.code.Code;
-import brainfuck.java.lang.compiler.intermediate_generator.IntermediateGenerator;
+import brainfuck.java.lang.compiler.intermediate_builder.IntermediateBuilder;
 
 public class BFCompiler {
 
     public void compile(Code sourceCode) {
-        IntermediateGenerator ig = new IntermediateGenerator();
-        System.out.println(ig.generateIntermediate(sourceCode));
     }
 
 }
